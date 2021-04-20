4f2cd624f6f203aa7633ecfc1eadd048f274aee4
PodburtnyiMaxim <mpodburtnyy@mail.ru>
2021-04-20 21:22:30 +0500

    Initialize

---
+++ main.py
@@ -0,0 +1,30 @@
+import decimal
+
+
+def napr(x,y):
+    result = (y+3*(x-1)-2)/x
+    return result
+
+def euler(n_x,x,y):
+    global x0,y0
+    k = napr(x,y)
+    y2 = k*(n_x-x)+y
+    return y2
+
+def float_range(start, stop, step):
+  while start < stop:
+    yield float(start)
+    start += decimal.Decimal(step)
+
+dots = [[1],[1]]
+x0 = dots[0][0]
+y0 = dots[1][0]
+
+for dot in float_range(1,4,0.5):
+    y = euler(dot,dots[0][-1],dots[1][-1])
+    dots[0].append(dot)
+    dots[1].append(y)
+
+dots[0].remove(1)
+dots[1].remove(1)
+print(dots[])
